e6127a3673601a5b726f4f665de6477b62880cef
subekk <agussubekti644@gmail.com>
2026-05-08 07:27:56 +0700

    fixed bugs-3

--- src/server.js
+++ src/server.js
@@ -1,19 +1,21 @@
-import express from 'express';
-import routes from './routes.js';
+import express from 'express';
+import routes from './routes.js';
 import cors from 'cors';
 
-const app = express();
-const port = process.env.PORT || 3000;
-const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
+const app = express();
+const port = process.env.PORT || 3000;
+const host = process.env.NODE_ENV !== 'production'
+  ? 'localhost'
+  : '0.0.0.0';
 
 app.use(express.json());
+
 app.use(cors({
   origin: '*'
 }));
 
-app.use('/', routes);
+app.use('/', routes);
 
-
-app.listen(port, () => {
- console.log(`Server running at http://${host}:${port}`);
-}); 
+app.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}`);
+});
